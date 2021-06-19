fd019f680b8e7e1ed29645e557632d5f7f2c573a
Biswajit Paul <biswajitkmrpl46@gmail.com>
2021-06-20 05:17:25 +0530

    Jump functionality added to Dino.

--- lib/Game/dino.dart
+++ lib/Game/dino.dart
@@ -1,7 +1,20 @@
+import 'dart:ui';
+
 import 'package:flame/components/animation_component.dart';
 import 'package:flame/spritesheet.dart';
+import 'package:flutter/material.dart';
+import 'package:flame/animation.dart' as Anim;
+
+const groudHeight = 15;
+const ratioOfDinoSize = 10;
+const GRAVITY = 1000.0;
 
 class Dino extends AnimationComponent {
+  Anim.Animation _runanimation;
+  Anim.Animation _hitanimation;
+  double yinitial = 0.0;
+  double speedY = 0.0;
+
   Dino() : super.empty() {
     final spriteImage = SpriteSheet(
       imageName: 'DinoSprites - vita.png',
@@ -10,22 +23,60 @@ class Dino extends AnimationComponent {
       columns: 24,
       rows: 1,
     );
-    final idelAnimation = spriteImage.createAnimation(
+    _hitanimation = spriteImage.createAnimation(
       0,
-      from: 0,
-      to: 3,
+      from: 14,
+      to: 16,
       stepTime: 0.1,
     );
-    final runAnimation = spriteImage.createAnimation(
+    _runanimation = spriteImage.createAnimation(
       0,
       from: 4,
       to: 10,
       stepTime: 0.1,
     );
-    this.animation = runAnimation;
-    this.height = 80;
-    this.width = 80;
+
+    this.animation = _runanimation;
+
     this.x = 100;
     this.y = 300;
+  }
+
+  @override
+  void update(double t) {
+    super.update(t);
+    // Max Height
+    //v = u + at;
+    this.speedY += GRAVITY * t;
+    // d=s*t
+    this.y += this.speedY * t;
+    if (isOnGround()) {
+      this.y = this.yinitial;
+      this.speedY = 0.0;
+    }
+  }
+
+  @override
+  void resize(Size size) {
+    super.resize(size);
+    this.height = this.width = size.width / ratioOfDinoSize;
+    this.y = size.height - this.height - groudHeight;
+    this.yinitial = this.y;
+  }
+
+  bool isOnGround() {
+    return (this.y >= yinitial);
+  }
+
+  void run() {
+    this.animation = _runanimation;
+  }
+
+  void hit() {
+    this.animation = _hitanimation;
+  }
+
+  void jump() {
+    if (isOnGround()) this.speedY = -600;
   }
 }
--- lib/Game/parallax_backgroud.dart
+++ lib/Game/parallax_backgroud.dart
@@ -1,9 +1,10 @@
 import 'package:dino_world/Game/dino.dart';
 import 'package:flame/components/parallax_component.dart';
 import 'package:flame/game/base_game.dart';
+import 'package:flame/gestures.dart';
 import 'package:flutter/cupertino.dart';
 
-class DinoGame extends BaseGame {
+class DinoGame extends BaseGame with TapDetector {
   Dino _dino = Dino();
   ParallaxComponent parallaxComponent;
 
@@ -19,5 +20,11 @@ class DinoGame extends BaseGame {
     );
     add(parallaxComponent);
     add(_dino);
+  }
+
+  @override
+  void onTapDown(TapDownDetails details) {
+    super.onTapDown(details);
+    _dino.jump();
   }
 }
